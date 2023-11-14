5f81dd707a287197406e66ced93283fd50c82640
Danil_V <Rain2195@gmail.com>
2023-11-14 15:17:08 +0300

    Часть 2: Добавил фильтры (Финальный коммит)

--- OrderEase.WebClient/Program.cs
+++ OrderEase.WebClient/Program.cs
@@ -1,6 +1,13 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+namespace OrderEase.WebClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-
-app.Run();
+            app.Run();
+        }
+    }
+}
